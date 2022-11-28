d687b61829bd9ddafaee44b80c0da8b94681e0c5
Francisco Ayala Le Brun <frankxlebrun@gmail.com>
2022-11-28 10:45:01 +0100

    Use typescript instead of js; Add sql.js dependency

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import _ from "lodash";
+import * as _ from "lodash";
 import * as d3 from "d3";
 
 function component() {
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,22 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
